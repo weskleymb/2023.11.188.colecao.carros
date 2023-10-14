cd195134139a7c74c0c51d0466536ae77bc55f45
Weskley Bezerra <weskleymb@hotmail.com>
2023-10-14 12:12:56 -0300

    aula de revisao

--- carros/urls.py
+++ carros/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from carros.views import index, form, delete
+from carros.views import index, form, delete, edit, nao_encontrado
 
 urlpatterns = [
     path('', index, name='index'),
+    path('nao_encontrado', nao_encontrado, name='nao_encontrado'),
     path('cadastro', form, name='form'),
     path('remover/<int:id>', delete, name='delete'),
+    path('editar/<int:id>', edit, name='edit'),
 ]
--- carros/views.py
+++ carros/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from carros.models import Carro
 from carros.forms import CarroForms
@@ -41,3 +41,31 @@ def delete(request, id):
     carro.delete()
     # redirecionar para a página inicial
     return redirect('index')
+
+# criar rota para a página não encontrado
+def nao_encontrado(request):
+    # renderizar o template de página não encontrado
+    return render(request, 'carros/nao_encontrado.html')
+
+# criar rota para a página de edição
+def edit(request, id):
+    # buscar o carro no banco de dados
+    carro = get_object_or_404(Carro, id=id)
+    # verificar se o carro existe
+    if not carro:
+        # redirecionar para a página nao encontrado
+        return redirect('nao_encontrado')
+    # iniciar o formulário
+    form = CarroForms(instance=carro)
+    # verificar se o formulário foi enviado
+    if request.method == 'POST':
+        # preencher o formulário com os dados enviados
+        form = CarroForms(request.POST, instance=carro)
+        # verificar se os dados são válidos
+        if form.is_valid():
+            # salvar os dados no banco de dados
+            form.save()
+            # redirecionar para a página inicial
+            return redirect('index')
+    # renderizar o template do formulário
+    return render(request, 'carros/edit.html', {'form': form, 'carro': carro})
